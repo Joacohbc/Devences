ce581f04a4055c91f2f39764b1082e61eae8f574
EmiDS17 <emiliano.ds1703@gmail.com>
2021-09-06 19:02:22 -0300

    Rediseño Formulario de Registro de Reservas

--- Capa de Presentacion/Formularios/Reservas/frmRegReservas.Designer.cs
+++ Capa de Presentacion/Formularios/Reservas/frmRegReservas.Designer.cs
@@ -56,15 +56,17 @@
             this.label3 = new System.Windows.Forms.Label();
             this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
             this.chkConfirmada = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvIntegrantes)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(406, 25);
+            this.label1.Location = new System.Drawing.Point(12, 14);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(175, 20);
             this.label1.TabIndex = 1;
@@ -72,9 +74,11 @@
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(913, 12);
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(917, 7);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.Size = new System.Drawing.Size(75, 32);
             this.btnSalir.TabIndex = 4;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
@@ -109,9 +113,11 @@
             // 
             // btnBorrarIntegrante
             // 
+            this.btnBorrarIntegrante.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBorrarIntegrante.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBorrarIntegrante.Location = new System.Drawing.Point(867, 128);
             this.btnBorrarIntegrante.Name = "btnBorrarIntegrante";
-            this.btnBorrarIntegrante.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrarIntegrante.Size = new System.Drawing.Size(90, 28);
             this.btnBorrarIntegrante.TabIndex = 89;
             this.btnBorrarIntegrante.Text = "Borrar";
             this.btnBorrarIntegrante.UseVisualStyleBackColor = true;
@@ -158,9 +164,11 @@
             // 
             // btnDescartar
             // 
+            this.btnDescartar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDescartar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDescartar.Location = new System.Drawing.Point(303, 406);
             this.btnDescartar.Name = "btnDescartar";
-            this.btnDescartar.Size = new System.Drawing.Size(87, 23);
+            this.btnDescartar.Size = new System.Drawing.Size(92, 36);
             this.btnDescartar.TabIndex = 87;
             this.btnDescartar.Text = "Descartar";
             this.btnDescartar.UseVisualStyleBackColor = true;
@@ -168,9 +176,11 @@
             // 
             // btnRegistrar
             // 
+            this.btnRegistrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRegistrar.Location = new System.Drawing.Point(197, 406);
             this.btnRegistrar.Name = "btnRegistrar";
-            this.btnRegistrar.Size = new System.Drawing.Size(100, 23);
+            this.btnRegistrar.Size = new System.Drawing.Size(92, 36);
             this.btnRegistrar.TabIndex = 86;
             this.btnRegistrar.Text = "Registrar";
             this.btnRegistrar.UseVisualStyleBackColor = true;
@@ -178,9 +188,11 @@
             // 
             // btnAgregarIntegrante
             // 
-            this.btnAgregarIntegrante.Location = new System.Drawing.Point(585, 191);
+            this.btnAgregarIntegrante.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregarIntegrante.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregarIntegrante.Location = new System.Drawing.Point(603, 189);
             this.btnAgregarIntegrante.Name = "btnAgregarIntegrante";
-            this.btnAgregarIntegrante.Size = new System.Drawing.Size(128, 23);
+            this.btnAgregarIntegrante.Size = new System.Drawing.Size(90, 28);
             this.btnAgregarIntegrante.TabIndex = 85;
             this.btnAgregarIntegrante.Text = "Agregar";
             this.btnAgregarIntegrante.UseVisualStyleBackColor = true;
@@ -196,9 +208,10 @@
             // 
             // txtCedulaIntegrante
             // 
-            this.txtCedulaIntegrante.Location = new System.Drawing.Point(555, 128);
+            this.txtCedulaIntegrante.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCedulaIntegrante.Location = new System.Drawing.Point(555, 116);
             this.txtCedulaIntegrante.Name = "txtCedulaIntegrante";
-            this.txtCedulaIntegrante.Size = new System.Drawing.Size(184, 20);
+            this.txtCedulaIntegrante.Size = new System.Drawing.Size(184, 26);
             this.txtCedulaIntegrante.TabIndex = 83;
             this.txtCedulaIntegrante.TextChanged += new System.EventHandler(this.txtCedulaIntegrante_TextChanged);
             this.txtCedulaIntegrante.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCedulaIntegrante_KeyPress);
@@ -206,18 +219,20 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(453, 131);
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(509, 70);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(96, 13);
+            this.label8.Size = new System.Drawing.Size(145, 20);
             this.label8.TabIndex = 82;
             this.label8.Text = "Cedula Integrantes";
             // 
             // lblTipoDeIng
             // 
             this.lblTipoDeIng.AutoSize = true;
-            this.lblTipoDeIng.Location = new System.Drawing.Point(142, 160);
+            this.lblTipoDeIng.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoDeIng.Location = new System.Drawing.Point(93, 124);
             this.lblTipoDeIng.Name = "lblTipoDeIng";
-            this.lblTipoDeIng.Size = new System.Drawing.Size(28, 13);
+            this.lblTipoDeIng.Size = new System.Drawing.Size(39, 20);
             this.lblTipoDeIng.TabIndex = 80;
             this.lblTipoDeIng.Tag = "";
             this.lblTipoDeIng.Text = "Tipo";
@@ -225,35 +240,39 @@
             // lblFechFin
             // 
             this.lblFechFin.AutoSize = true;
-            this.lblFechFin.Location = new System.Drawing.Point(142, 216);
+            this.lblFechFin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechFin.Location = new System.Drawing.Point(93, 180);
             this.lblFechFin.Name = "lblFechFin";
-            this.lblFechFin.Size = new System.Drawing.Size(21, 13);
+            this.lblFechFin.Size = new System.Drawing.Size(31, 20);
             this.lblFechFin.TabIndex = 79;
             this.lblFechFin.Text = "Fin";
             // 
             // lblFechIn
             // 
             this.lblFechIn.AutoSize = true;
-            this.lblFechIn.Location = new System.Drawing.Point(141, 189);
+            this.lblFechIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechIn.Location = new System.Drawing.Point(92, 153);
             this.lblFechIn.Name = "lblFechIn";
-            this.lblFechIn.Size = new System.Drawing.Size(32, 13);
+            this.lblFechIn.Size = new System.Drawing.Size(46, 20);
             this.lblFechIn.TabIndex = 78;
             this.lblFechIn.Text = "Inicio";
             // 
             // lblCedTit
             // 
             this.lblCedTit.AutoSize = true;
-            this.lblCedTit.Location = new System.Drawing.Point(116, 136);
+            this.lblCedTit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCedTit.Location = new System.Drawing.Point(67, 100);
             this.lblCedTit.Name = "lblCedTit";
-            this.lblCedTit.Size = new System.Drawing.Size(72, 13);
+            this.lblCedTit.Size = new System.Drawing.Size(106, 20);
             this.lblCedTit.TabIndex = 77;
             this.lblCedTit.Text = "Cédula Titular";
             // 
             // txtCedulaTitular
             // 
-            this.txtCedulaTitular.Location = new System.Drawing.Point(197, 133);
+            this.txtCedulaTitular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCedulaTitular.Location = new System.Drawing.Point(197, 121);
             this.txtCedulaTitular.Name = "txtCedulaTitular";
-            this.txtCedulaTitular.Size = new System.Drawing.Size(193, 20);
+            this.txtCedulaTitular.Size = new System.Drawing.Size(193, 26);
             this.txtCedulaTitular.TabIndex = 75;
             this.txtCedulaTitular.TextChanged += new System.EventHandler(this.txtCedulaTitular_TextChanged);
             this.txtCedulaTitular.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCedulaTitular_KeyPress);
@@ -274,9 +293,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(521, 163);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(472, 127);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.Size = new System.Drawing.Size(39, 20);
             this.label2.TabIndex = 96;
             this.label2.Tag = "";
             this.label2.Text = "Tipo";
@@ -304,11 +324,24 @@
             this.chkConfirmada.Text = "Confirmada";
             this.chkConfirmada.UseVisualStyleBackColor = true;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnSalir);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1000, 47);
+            this.panel1.TabIndex = 99;
+            // 
             // frmRegReservas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
             this.ClientSize = new System.Drawing.Size(1000, 605);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnAgregarIntegrante);
             this.Controls.Add(this.chkConfirmada);
             this.Controls.Add(this.label3);
@@ -329,14 +362,14 @@
             this.Controls.Add(this.lblFechIn);
             this.Controls.Add(this.lblCedTit);
             this.Controls.Add(this.txtCedulaTitular);
-            this.Controls.Add(this.btnSalir);
-            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmRegReservas";
             this.Text = "frmRegReservas";
             this.Load += new System.EventHandler(this.frmRegReservas_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvIntegrantes)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -371,5 +404,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Apellido;
         private System.Windows.Forms.DataGridViewTextBoxColumn Genero;
         private System.Windows.Forms.DataGridViewTextBoxColumn tipoDeIngreso;
+        private System.Windows.Forms.Panel panel1;
     }
 }
